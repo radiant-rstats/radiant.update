9bdffc5844cf3091b3996a6631390d7dc240a7b5
vnijs <radiant@rady.ucsd.edu>
2018-04-01 13:17:46 -0700

    update

--- R/radiant.R
+++ R/radiant.R
@@ -15,17 +15,27 @@ radiant.update <- function() {
 
   ## command to run after R session is restarted
   cmd <- "source('https://raw.githubusercontent.com/radiant-rstats/minicran/gh-pages/update.R')"
+  mess <- paste0("Some packages are already loaded. Please restart R (Rstudio) and run radiant.update::radiant.update() again")
 
-  ## check if run from Rstudio
-  if (rstudioapi::isAvailable()) {
-    message("\nUpdating Radiant. Your R session will now restart ...")
-    ## Restarting Rstudio session from http://stackoverflow.com/a/25934774/1974918
-    if (rstudioapi::versionInfo()$version >= "1.1.383") {
-      rstudioapi::restartSession(cmd)
-    } else {
-      ret <- .rs.restartR(cmd)
+  if (length(search()) < 11) {
+    ret <- try(source('https://raw.githubusercontent.com/radiant-rstats/minicran/gh-pages/update.R'), silent = FALSE)
+    if (is(ret, "try-error")) {
+      message(mess)
     }
   } else {
-    message("Please restart R, copy and paste the 'source' command below into the R console, and press return\n\n", cmd)
+    ## check if run from Rstudio
+    if (rstudioapi::isAvailable()) {
+      message("\nUpdating Radiant. Your R session will now restart ...")
+      ## Restarting Rstudio session from http://stackoverflow.com/a/25934774/1974918
+      if (rstudioapi::versionInfo()$version >= "1.1.383") {
+        rstudioapi::restartSession(cmd)
+        # rstudioapi::restartSession("radiant.update::radiant.update()")
+      } else {
+        ret <- .rs.restartR(cmd)
+        # ret <- .rs.restartR("radiant.update::radiant.update()")
+      }
+    } else {
+      message(mess)
+    }
   }
 }
